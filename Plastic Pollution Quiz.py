633698892f24a2e1033b1b6f853351655ad7709a
phillipsh1 <phillipsh@stu.otc.school.nz>
2025-05-16 10:35:32 +1200

    Added an introduction and rerun feature

--- Plastic Pollution Quiz.py
+++ Plastic Pollution Quiz.py
@@ -1,3 +1,10 @@
 '''Quiz  Program on Plastic Pollution by Hunter Phillips'''
 
-print("Hello world")
+rerun = True
+while rerun:
+#Rerun allows the code to be repeated. This function may be disabled by the user
+#at the end of the quiz when asked whether they would like to repeat it or not.
+#If the user doesn't want to repeat the quiz, rerun will become false ending the
+#loop.
+    print("Hello and welcome to this quiz about plastic pollution. It will consist of N questions that will test your knowledge and help you learn more about plastic pollution.")
+    
