b8dfd9ef754bdfd8eea7d4694f4223bb7911881e
phillipsh1 <phillipsh@stu.otc.school.nz>
2025-05-28 10:03:26 +1200

    'Added the 10 questions aswell as the options for the quiz.'

--- Plastic Pollution Quiz.py
+++ Plastic Pollution Quiz.py
@@ -6,5 +6,40 @@ while rerun:
 #at the end of the quiz when asked whether they would like to repeat it or not.
 #If the user doesn't want to repeat the quiz, rerun will become false ending the
 #loop.
-    print("Hello and welcome to this quiz about plastic pollution. It will consist of N questions that will test your knowledge and help you learn more about plastic pollution.")
+    print("Hello and welcome to this quiz about plastic pollution.\n" 
+    "It will consist of 10 questions that will test your knowledge and \n" 
+    "help you learn more about plastic pollution.")
+
+    ques = {
+    "What is the most common plastic waste found in the ocean?":
+        "a) Plastic bags b) Plastic straws c) Cigarette butts d) Fishing nets",
+    "Approximately how many metric tons of plastic enter the worlds oceans each year?":
+        "a) 800,000 b) 1 million c) 8 million d) 20 million",
+    "Which marine animals are most at risk from ingesting plastic debris?":
+        "a) Dolphins b) Sea turtles c) Whales d) All of the above",
+    "What is the Great Pacific Garbage Patch?":
+        "a) A new marine species b) An island made of sand c) A large area of floating plastic debris d) A coral reef",
+    "Which of the following is NOT a way plastic pollution harms wild life?":
+        "a) Entanglement b) Ingestion of plastic c) Providing shelter d) Chemical contamination",
+    "Microplastics are defined as plastic particles smaller than:":
+        "a) 1 meter b) 5 millimeters c) 1 centimeter d) 10 centimeters",
+    "What percentage of seabirds are estimated to have plastic in their stomachs?":
+        "a) 5% b) 20% c) 60% d) 90%",
+    "Which human activity contributes most to plastic pollution in oceans?":
+        "a) Fishing b) Littering on land c) Oil drilling c) Whale watching",
+    "What are microplastics?":
+        "a) Large plastic objects b) Tiny plastic particles formed from the breakdown of larger plastics c) A type of fish d) Natural ocean minerals",
+    "Which international agreement aims to reduce plastic waste in the oceans?":
+        "a) Paris agreement b) MARPOL Convention c) Antartic Treaty d) Geneva Convention"
+}
+    print(ques)
     
+    
+    
+    
+    
+    
+    
+    
+    
+    ans=input("Please enter your answer here: ")
