918e5176a923ee5db5c0483a433c5ff1f338e9b2
phillipsh1 <phillipsh@stu.otc.school.nz>
2025-08-01 10:27:31 +1200

    'Added option for users to add their own questions'

--- Plastic Pollution Quiz.py
+++ Plastic Pollution Quiz.py
@@ -1,19 +1,7 @@
-'''Quiz  Program on Plastic Pollution by Hunter Phillips'''
+'''Quiz Program on Plastic Pollution by Hunter Phillips'''
 
-rerun = True
-while rerun:
-#Rerun allows the code to be repeated. This function may be disabled by the user
-#at the end of the quiz when asked whether they would like to repeat it or not.
-#If the user doesn't want to repeat the quiz, rerun will become false ending the
-#loop.
-    
-    print("Hello and welcome to this quiz about plastic pollution.\n" 
-    "It will consist of 10 questions that will test your knowledge and \n" 
-    "help you learn more about plastic pollution. When answering a question, you will be shown a list of possible answers. \n"
-    "Once you have chosen the answer you believe is correct, you then enter the answers corresponding letter (a,b,c or d)")
-    
-    #This dictionary contains the questions along with the users options
-    ques = {
+#This dictionary contains the questions along with the users options
+ques = {
     "1.": "What is the most common plastic waste found in the ocean? \n"
             "a) Plastic bags \n"
             "b) Plastic straws \n"
@@ -66,7 +54,7 @@ while rerun:
             "d) Geneva Convention"
 }
     #Contains the answers to each of the questions above
-    ans = {
+ans = {
     "1.":
         "C", # Cigarette butts
     "2.":
@@ -88,25 +76,43 @@ while rerun:
     "10.": 
         "B" # MARPOL Convention
 }
+
+rerun = True
+while rerun:
+#Rerun allows the code to be repeated. This function may be disabled by the user
+#at the end of the quiz when asked whether they would like to repeat it or not.
+#If the user doesn't want to repeat the quiz, rerun will become false ending the
+#loop.
+    
+    print("Hello and welcome to this quiz about plastic pollution.\n" 
+    "It will consist of 10 questions that will test your knowledge and \n" 
+    "help you learn more about plastic pollution. When answering a question, you will be shown a list of possible answers. \n"
+    "Once you have chosen the answer you believe is correct, you then enter the answers corresponding letter (a,b,c or d)")
+    
     #The list of valid options the user can choose from. When an invalid option (not in this list) is input, it isnt accepted.
-    valid=['A','B','C','D']
+    valid_ans=['A','B','C','D']
+    
+    amount=0
     points = 0
     correct = 0 
 
+    #Functions for when the user gets a question correct or incorrect. 
+    #They display to the user their answer (A,B,C or D) and whether they are correct or incorrect.
+    #The users points are also shown.
     def user_correct(x,y):
         print(f"You have selected the answer {x}")
-        print(f"{x} is correct! You now have {y} points! :D")
+        print(f"{x} is correct! You now have {y} points!")
     
     def user_wrong(x,y):
         print(f"You have selected the answer {x}")
-        print(f"{x} is incorrect! You still have {y} points :(")
+        print(f"{x} is incorrect! You still have {y} points")
         print("Better luck on your next question!!")
     #The proccess of asking the user questions, getting their response, and seeing if they are correct or incorrect.
     for key, value in ques.items():
         print(key, value)
         choice = input("Please enter your answer here: ")
         choice = choice.capitalize()
-        while choice not in valid:
+        while choice not in valid_ans:
             print("Your answer was not valid, your answer must be either: A, B, C, or D")
             choice = input("Please enter your answer here: ")
             choice = choice.capitalize()
@@ -117,7 +123,7 @@ while rerun:
         if choice != ans[key]:
             user_wrong(choice, points)
     
-    print(f"Thank you for taking this quiz! You were able to get a total of {points}/10 points, which is {correct} answers correct.")
+    print(f"Thank you for taking this quiz! You were able to get a total of {points} points, which is {correct} answers correct.")
     #The code below is responsible for the rerun function located at the top of the code.
     #The user has the option to either repeat the quiz or end it. 
     #The code will repeat if the user enters "Yes" and will end if the user enters "No"
@@ -127,24 +133,44 @@ while rerun:
         retake = input("That is an invalid answer. Please answer with 'Yes' or 'No': ")
     
     if retake == "yes" or retake == "Yes":
-        print("Goodluck on your next attempt! ^_^")
+        print("Goodluck on your next attempt!")
         rerun = True
     else:  
         rerun = False 
-        print("Goodbye! O_< ")   
+        print("Goodbye!")   
 
-        print("Now that you have successfully completed the given questions, you may now submit your own question.")
-        user_choice = input("Do you wish to add your own question to the quiz? Answer with 'Yes' or 'No'")
-        while user_choice not in ["yes", "no", "Yes", "No"]:
-            print("That is an invalid answer. Please answer again with either 'Yes' or 'No'")
-            user_choice = input("Do you wish to add your own question to the quiz? Answer with 'Yes' or 'No'")
-        while user_choice == ['yes', 'Yes']:
-            user_ques = input("Please enter your selected question: ")
-            
-            user_ans = input("Enter your questions correct answer: ")
-            user_opt = input("Enter option A: ")
-            user_opt = input("Enter option B: ")
-            user_opt = input("Enter option C: ")
-            user_opt = input("Enter option D: ")
-            user_choice = input("Do you want to create another question?")
-        
+
+    valid_num=['1','2','3','4']
+    valid_response=['Yes','yes','No','no']
+
+    print("Now that you have successfully completed the given questions, you may now submit your own question.")
+    user_choice = input("Do you wish to add your own question to the quiz? Answer with 'Yes' or 'No': ")
+    user_choice = user_choice.strip().lower()
+    while user_choice not in valid_response:
+        print("That is an invalid answer. Please answer again with either 'Yes' or 'No': ")
+        user_choice = input("Do you wish to add your own question to the quiz? Answer with 'Yes' or 'No': ")
+        user_choice = user_choice.strip().lower()
+    print(user_choice)
+    if user_choice == 'yes':
+        user_ques = input("Please enter your selected question: ")
+        opt_A = input("Enter option A: ")
+        opt_B = input("Enter option B: ")
+        opt_C = input("Enter option C: ")
+        opt_D = input("Enter option D: ")
+        user_ans = input("Which of the 4 options given is the correct answer? (A, B, C, or D): ")
+        user_ans = user_ans.strip().upper()
+        print(user_ans)
+        while user_ans not in valid_ans:
+            print("That is an invalid answer. Please select a letter out of A, B, C, or D")
+            user_ans = input("Which of the 4 options given is the correct answer? (A, B, C, or D): ")
+            user_ans = user_ans.strip().upper()
+        user_choice = input("Do you want to create another question? Answer with 'Yes' or 'No' ")
+
+        new_ques = user_ques + "\n" + opt_A + "\n" + opt_B + "\n" + opt_C + "\n" + opt_D
+        new_ans = user_ans
+        print(new_ques)
+
+        ques.update({"11.": (new_ques)})
+        ans.update({"11.": (new_ans)})
+    else:
+        print("bye")
